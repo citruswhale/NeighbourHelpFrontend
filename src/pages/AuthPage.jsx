8f80269c414159b8c78d02dd6024d09d5cea0462
Trinity2206 <srinidhi2206@gmail.com>
2025-10-29 15:04:20 +0530

    password placeholder

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -173,7 +173,7 @@ const AuthPage = () => {
                     <input
                         type="password"
                         name="password"
-                        placeholder="••••••••"
+                        placeholder="password"
                         className="w-full border rounded-md px-3 py-2"
                         onChange={handleChange}
                         required
